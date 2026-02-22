79dc624bc018cd73fd0f0b4c918b7a1631eae5ae
Agung <agungsetyawa99@gmail.com>
2026-02-23 00:17:34 +0700

    UI: Fix bottom nav not perfectly centered due to transform override

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -50,7 +50,7 @@ const showBottomNav = computed(() => {
       </router-link>
     </transition>
     <!-- Bottom Mobile Navigation -->
-    <div v-if="showBottomNav" class="bottom-dock animate-fade-up">
+    <div v-if="showBottomNav" class="bottom-dock">
       <router-link to="/" class="dock-item" :class="{ 'router-link-active': route.name === 'home' }">
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
         <span class="dock-label">Home</span>
